76eb7d6c7c5bce043d118d788a7f62c1e8bcf4e5
Tmayess <mayess.tairi24@gmail.com>
2026-02-23 16:20:49 +0100

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <p class="text-red-500">hello</p>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
+  supabase: {
+    types: "~~/database.types.ts",
+    redirectOptions: {
+      login: '/login',
+      callback: '/confirm',
+      exclude: ['/'], 
+    }
+  },
+})
